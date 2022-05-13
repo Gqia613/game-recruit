bf1342ec264b5fabc0c41204c232fc0154464491
大城由晃 <yoshiaki19980217@icloud.com>
2022-05-14 01:15:07 +0900

    コード修正

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\Group;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Seeder;
 
 class GroupSeeder extends Seeder
 {
