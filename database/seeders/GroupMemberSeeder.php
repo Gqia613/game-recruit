f3a1e0dfa557bbcb9a92467c6f4574987b99a697
大城由晃 <yoshiaki19980217@icloud.com>
2022-05-15 18:35:02 +0900

    テストデータ修正

--- database/seeders/GroupMemberSeeder.php
+++ database/seeders/GroupMemberSeeder.php
@@ -25,60 +25,28 @@ class GroupMemberSeeder extends Seeder
                 'user_id'  => 1,
             ],
             [
-                'group_id' => 1,
-                'user_id'  => 2,
+                'group_id' => 2,
+                'user_id'  => 1,
             ],
             [
-                'group_id' => 2,
-                'user_id'  => 3,
+                'group_id' => 3,
+                'user_id'  => 1,
             ],
             [
-                'group_id' => 2,
+                'group_id' => 4,
                 'user_id'  => 4,
             ],
             [
-                'group_id' => 2,
+                'group_id' => 5,
                 'user_id'  => 5,
             ],
             [
-                'group_id' => 3,
+                'group_id' => 6,
                 'user_id'  => 6,
             ],
             [
-                'group_id' => 3,
+                'group_id' => 7,
                 'user_id'  => 7,
-            ],
-            [
-                'group_id' => 4,
-                'user_id'  => 8,
-            ],
-            [
-                'group_id' => 4,
-                'user_id'  => 9,
-            ],
-            [
-                'group_id' => 5,
-                'user_id'  => 10,
-            ],
-            [
-                'group_id' => 6,
-                'user_id'  => 11,
-            ],
-            [
-                'group_id' => 6,
-                'user_id'  => 12,
-            ],
-            [
-                'group_id' => 7,
-                'user_id'  => 13,
-            ],
-            [
-                'group_id' => 7,
-                'user_id'  => 14,
-            ],
-            [
-                'group_id' => 7,
-                'user_id'  => 15,
             ],
         ];
 
